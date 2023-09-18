58c94cc986190f6e4619dcce9876516c0fd1f08f
unknown <work.ruslan.tkachenko@gmail.com>
2023-09-18 18:05:25 +0300

    cleanup

--- web-socket-service.js
+++ web-socket-service.js
@@ -1,50 +1,26 @@
 import * as ws from 'ws';
 
 export class WebSocketSerice {
-    wsClient;
     wsServer;
 
     constructor(port) {
-        // console.log(ws);
-        // console.log(ws.Server);
-        // console.log(ws.WebSocketServer);
-
         this.wsServer = new ws.WebSocketServer({ port, });
-        // this.wsServer = new ws.Server({ port, });
-
-        // console.log(this.wsServer)
-
         this.wsServer.on('connection', this.onConnect.bind(this));
-
-        // this.wsServer.addListener('connection', this.onConnect.bind(this))
     }
 
     onConnect(wsClient) {
-        // console.log('wsClient', wsClient)
-
-
-        console.log('Новый пользователь');
-        // отправка приветственного сообщения клиенту
         wsClient.send('Привет');
         wsClient.on('message', (message) => {
-            /* обработчик сообщений от клиента */
-            console.log('m', message.toString())
-            // wsClient.send(message.toString());
-
-            // this.wsServer.broadcast = function (data) {
+            console.log('m', message.toString());
             this.wsServer.clients.forEach(client => client.send(message.toString()));
-            // };
         });
         wsClient.on('close', function () {
-            // отправка уведомления в консоль
             console.log('Пользователь отключился');
         });
 
         this.wsClient = wsClient;
 
         if (this.reaction) this.reaction();
-
-
     }
 
     /**
@@ -52,7 +28,7 @@ export class WebSocketSerice {
      * @param {(message: any??) => void} cb
      */
     onMessage(cb) {
-        this.wsClient.on('message', cb);
+
     }
 
     /**
@@ -60,11 +36,8 @@ export class WebSocketSerice {
      * @param {any?} message
      */
     sendMessage(message) {
-        this.wsClient.send(message);
+        this.wsServer.clients.forEach(client => client.send(message));
     }
-
-
-
 
     testReaction(cb) {
         this.reaction = cb;
