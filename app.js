05d59ea4c31af27c3960f7e99c3cdec75756d951
unknown <work.ruslan.tkachenko@gmail.com>
2023-09-18 18:22:09 +0300

    test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ export const app = {
     init(port = 1234) {
         const webSocket = new WebSocketSerice(port)
             .testReaction(
-                () => setTimeout(() => webSocket.sendMessage('delay message test'), 5000),
+                () => setTimeout(() => webSocket.sendMessage('delay message test'), 3000),
             );
 
     }
